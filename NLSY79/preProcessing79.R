febe0a466407e85ec4d8a83a75c45e341f62fc4d
MarinaWyss <marina.bennett.wyss@gmail.com>
2019-10-05 17:25:57 +0200

    updating pre-processing 1979

--- NLSY79/preProcessing79.R
+++ NLSY79/preProcessing79.R
@@ -4,5 +4,5 @@ varNames <- read.csv("varNames.csv", header = FALSE)
 
 names(trainData79) <- varNames$V2
 
-
+tinySample <- sample(trainData79, size = 100)
 
--- preProcessing79.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-
-varNames <- read.csv("varNames.csv", header = FALSE)
-
-names(trainData79) <- varNames$V2
-
-tinySample <- sample(trainData79, size = 100)
-
