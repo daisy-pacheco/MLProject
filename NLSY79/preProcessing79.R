4b7668715d24ab98925570c4a117c8fb82ec93e8
MarinaWyss <marina.bennett.wyss@gmail.com>
2019-10-06 08:25:41 +0200

    documenting and fixing tinySample79

--- NLSY79/preProcessing79.R
+++ NLSY79/preProcessing79.R
@@ -1,8 +1,23 @@
 library(dplyr)
+library(caret)
 
+# switching variable names to words vs codes
+# *** THIS PROCESS SHOULD BE IMPROVED ***
 varNames <- read.csv("varNames.csv", header = FALSE)
 
 names(trainData79) <- varNames$V2
 
-tinySample79 <- sample(trainData79, size = 100)
+# making a tiny sample for testing feature engineering 
+set.seed(456) 
+
+index <- createDataPartition(trainData79$CASEID, p = 0.99, 
+                             list = FALSE)
+
+tinySample79  <- trainData79[-index, ]
+
+### FEATURE ENGINEERING ###
+
+# new column: average tenure per person per year
+tinySample79$avgTenure <- tinySample79 %>%
+  mutate(avgTenure = mean())
 
