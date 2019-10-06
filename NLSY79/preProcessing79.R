f13283d898de2f4ca2dd8219ae3b8d4531ed5394
Marina Wyss <47151798+MarinaWyss@users.noreply.github.com>
2019-10-06 11:47:18 +0200

    fixing names matching

--- NLSY79/preProcessing79.R
+++ NLSY79/preProcessing79.R
@@ -1,10 +1,10 @@
 library(dplyr)
 
 # switching variable names to words vs codes
-# *** THIS PROCESS SHOULD BE IMPROVED ***
 varNames <- read.csv("varNames.csv", header = FALSE)
 
-names(trainData79) <- varNames$V3
+names(trainData79) <- varNames[match(names(trainData79), varNames[ ,'V1']),'V3']
+
 
 # making a tiny sample for testing feature engineering 
 set.seed(456) 
@@ -13,4 +13,13 @@ tinySample79 <- sample_n(trainData79, 20)
 
 
 ### FEATURE ENGINEERING ###
+
+# finding most recent job for 1979 - 1993
+
+stopweeksTrain <- trainData79 %>% 
+  select(CASEID_R0000100, starts_with('EMPLOYERS_ALL_STOPWEEK'))
+
+
+
+
 
