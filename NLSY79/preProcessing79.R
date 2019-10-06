9bef3b5be8b98f8b7e4db9ae59a41efbfdd045b2
MarinaWyss <marina.bennett.wyss@gmail.com>
2019-10-06 08:36:41 +0200

    Better names and sampling

--- NLSY79/preProcessing79.R
+++ NLSY79/preProcessing79.R
@@ -1,23 +1,24 @@
 library(dplyr)
-library(caret)
 
 # switching variable names to words vs codes
 # *** THIS PROCESS SHOULD BE IMPROVED ***
 varNames <- read.csv("varNames.csv", header = FALSE)
 
-names(trainData79) <- varNames$V2
+names(trainData79) <- varNames$V3
 
 # making a tiny sample for testing feature engineering 
 set.seed(456) 
 
-index <- createDataPartition(trainData79$CASEID, p = 0.99, 
-                             list = FALSE)
+tinySample79 <- sample_n(trainData79, 20)
 
-tinySample79  <- trainData79[-index, ]
 
 ### FEATURE ENGINEERING ###
 
 # new column: average tenure per person per year
 tinySample79$avgTenure <- tinySample79 %>%
   mutate(avgTenure = mean())
+
+
+tinySample79 %>% 
+  select(starts_with("TENURE"))
 
