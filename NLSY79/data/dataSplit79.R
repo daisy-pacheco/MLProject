1aa3b670fe561525a7ae20314729e6ce1ffb7194
Marina Wyss <47151798+MarinaWyss@users.noreply.github.com>
2019-10-09 07:04:39 +0200

    Adding 2014 and 2016 age, filtering working dataset

--- NLSY79/data/dataSplit79.R
+++ NLSY79/data/dataSplit79.R
@@ -1,5 +1,4 @@
 library(caret)
-library(dplyr)
 
 set.seed(456) 
 index <- createDataPartition(new_data$R0000100, p = 0.7, 
@@ -9,4 +8,6 @@ testData79  <- new_data[-index, ]
 
 save(trainData79, file = "trainData79.RData")
 save(testData79, file = "testData79.RData")
+
+
 
