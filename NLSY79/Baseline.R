6114c7c27500cad57151046deadeffbebb2ed0ef
Daisy Pacheco <dpachecobernal@gmail.com>
2019-10-11 12:13:29 +0200

    Baseline regressions 80,87,06

--- NLSY79/Baseline.R
+++ NLSY79/Baseline.R
@@ -89,18 +89,76 @@ just1stJob80 <- just1stJob80 %>%
   filter(!is.na(JOB_SATISFACTION.01_1980),
          NET_FAMILY_INCOME_R0406010 != "-Inf")
 
-model1 <- lm(JOB_SATISFACTION.01_1980 ~ MEAN_AGE_JOB1_1980 + 
+model1_80 <- lm(JOB_SATISFACTION.01_1980 ~ MEAN_AGE_JOB1_1980 + 
                TENURE1_R0333221 + 
                EMPLOYERS_ALL_HRLY_WAGE_1980.01_E8070100, 
              data = just1stJob80)
 
-summary(model1)
+summary(model1_80)
 
-model2 <- lm(JOB_SATISFACTION.01_1980 ~ ., data = just1stJob80)
+model2_80 <- lm(JOB_SATISFACTION.01_1980 ~ ., data = just1stJob80)
 
-summary(model2)
+summary(model2_80)
 
+# 1987
 
+just1stJob87 <- workingTrainData79_1stJob %>%
+  select(JOB_SATISFACTION.01_1987, 
+         MEAN_AGE_JOB1_1987, 
+         TENURE1_R2372510,
+         EMPLOYERS_ALL_HRLY_WAGE_1987.01_E8140100,
+         ROSENBERG_IRT_SCORE_R2350020,
+         ROTTER_SCORE_R0153710, # 1979
+         HIGHEST_GRADE_COMPLETED_R2306500,
+         `R_REL-1_COL_R0010300`,
+         NET_FAMILY_INCOME_R2444700,
+         `URBAN-RURAL_R2446900`,
+         EMPLOYERS_ALL_UNION_1987.01_E6700100,
+         PUBLIC.01_1987)
+
+just1stJob87 <- just1stJob87 %>%
+  filter(!is.na(JOB_SATISFACTION.01_1987))
+
+model1_87 <- lm(JOB_SATISFACTION.01_1987 ~ MEAN_AGE_JOB1_1987 + 
+                  TENURE1_R2372510 + 
+                  EMPLOYERS_ALL_HRLY_WAGE_1987.01_E8140100, 
+                data = just1stJob87)
+
+summary(model1_87)
+
+model2_87 <- lm(JOB_SATISFACTION.01_1987 ~ ., data = just1stJob87)
+
+summary(model2_87)
+
+# 2006
+
+just1stJob06 <- workingTrainData79_1stJob %>%
+  select(JOB_SATISFACTION.01_T0277600, 
+         MEAN_AGE_JOB1_2006, 
+         TENURE1_T0986300,
+         EMPLOYERS_ALL_HRLY_WAGE_2006.01_E8270100,
+         ROSENBERG_IRT_SCORE_T0899820,
+         ROTTER_SCORE_R0153710, # 1979
+         HIGHEST_GRADE_COMPLETED_T0014400,
+         `R_REL-1_COL_R0010300`,
+         NET_FAMILY_INCOME_T0987800,
+         `URBAN-RURAL_T0990400`,
+         EMPLOYERS_ALL_UNION_2006.01_E6830100,
+         PUBLIC.01_2006)
+
+just1stJob06 <- just1stJob06 %>%
+  filter(!is.na(JOB_SATISFACTION.01_T0277600))
+
+model1_06 <- lm(JOB_SATISFACTION.01_T0277600 ~ MEAN_AGE_JOB1_2006 + 
+                  TENURE1_T0986300 + 
+                  EMPLOYERS_ALL_HRLY_WAGE_2006.01_E8270100, 
+                data = just1stJob06)
+
+summary(model1_06)
+
+model2_06 <- lm(JOB_SATISFACTION.01_T0277600 ~ ., data = just1stJob06)
+
+summary(model2_06)
 
 ### data viz ###
 plot1980 <- ggplot(aes(x = as.factor(JOB_SATISFACTION.01_1980)), 
