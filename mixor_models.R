18f9c8655123d6f71a5bdabed2885e32cc1b5c19
Marina Wyss <marina.wyss@inwt-statistics.de>
2019-11-07 16:37:42 +0100

    updating models and .RData for results

--- mixor_models.R
+++ mixor_models.R
@@ -1,68 +1,66 @@
 library(tidyverse)
-
-full_data <- prepped_data
+set.seed(123)
 
 # filtering for full time
 full_time_data <- prepped_data %>% 
-  filter(hours_worked_week >= 35)
+  filter(hours_worked_week >= 35) %>% 
+  select(-full_time)
 
 # filtering for public sector
 public_data <- full_time_data %>% 
-  filter(public == 1)
+  filter(public == 1) %>% 
+  select(-public)
 
 
-# models
+# mixor
 library(mixor)
-set.seed(123)
 
-# full time only
+## full time only
 full_time_model <- mixor(job_satisfaction ~
                            religion + ethnicity + gender + highest_grade + urban_rural + net_family_income_centered +
-                           job_number + industry + union + public + 
-                           hourly_pay_centered + avg_age_per_job_centered + tenure_centered + hours_worked_week_centered +
+                           job_number + union + public + 
+                           hourly_pay_centered + avg_age_per_job_centered + tenure_centered + 
                            rotter_score_centered + rosenberg_score_centered,
                          id = id, 
                          link = "logit",
                          data = full_time_data)
 
 summary(full_time_model)
-plot(full_time_model)
 
-# public sector full time only
+## public sector full time only
 public_model <- mixor(job_satisfaction ~
                         religion + ethnicity + gender + highest_grade + urban_rural + net_family_income_centered +
-                        job_number + industry + union + 
-                        hourly_pay_centered + avg_age_per_job_centered + tenure_centered + hours_worked_week_centered +
+                        job_number + union + 
+                        hourly_pay_centered + avg_age_per_job_centered + tenure_centered + 
                         rotter_score_centered + rosenberg_score_centered,
                       id = id, 
                       link = "logit",
                       data = public_data)
 
 summary(public_model)
-plot(public_model)
 
-
-# balanced class example 
+## balanced class example 
 library(caret)
-balanced_data <- downSample((x = full_time_data[ ,-job_satisfaction],
-                             y = full_time_data$job_satisfaction))
+full_time_data$job_satisfaction <- as.factor(full_time_data$job_satisfaction)
+balanced_data <- downSample(x = full_time_data,
+                             y = full_time_data$job_satisfaction)
+balanced_data <- balanced_data[order(balanced_data$id),]
 
 balanced_model <- mixor(job_satisfaction ~
                           religion + ethnicity + gender + highest_grade + urban_rural + net_family_income_centered +
-                          job_number + industry + union + full_time + public + 
-                          hourly_pay_centered + avg_age_per_job_centered + tenure_centered + hours_worked_week_centered +
+                          job_number + union + public + 
+                          hourly_pay_centered + avg_age_per_job_centered + tenure_centered + 
                           rotter_score_centered + rosenberg_score_centered,
                         id = id, 
                         link = "logit",
                         data = balanced_data)
 
 summary(balanced_model)
-plot(balanced_model)
 
 
 # predicting
-preds <- predict(model)
-cm <- table(preds$class, subsample$job_satisfaction)
+preds <- predict(full_time_model)
+cm <- table(preds$class, full_time_data$job_satisfaction)
 
 precrecall <- function(mytable, verbose=TRUE) {
   truePositives <- mytable[1, 1]
@@ -80,3 +78,15 @@ precrecall <- function(mytable, verbose=TRUE) {
 
 precrecall(cm) 
 sum(diag(cm)) / sum(cm)
+
+
+# kable table
+library(kableExtra)
+
+public_info <- public_model[4]
+full_time_info <- full_time_model[4]
+
+
+public_info %>% kable() %>% kable_styling()
+
+
