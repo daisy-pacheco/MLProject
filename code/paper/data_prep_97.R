0b017cb79ef8ac85365faa954a84e673288f7717
Daisy Pacheco <dpachecobernal@gmail.com>
2020-03-05 22:51:03 +0100

    dataprep97

--- code/paper/data_prep_97.R
+++ code/paper/data_prep_97.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-original_data <- read_csv('testdata.csv')
-names_dictionary <- read_csv('test-dict.csv')
+original_data_97 <- read_csv("data/paper/fulldata_97.csv")
+names_dictionary_97 <- read.csv("data/paper/dictionary_97.csv", sep = ";")
 
-tidy_data <- original_data %>% 
+tidy_data_97 <- original_data_97 %>% 
   tidyr::pivot_longer(
     -c(R0000100),
     names_to = "variable_id",
@@ -11,7 +11,7 @@ tidy_data <- original_data %>%
     values_drop_na = TRUE
   ) %>% 
   dplyr::left_join(
-    names_dictionary
+    names_dictionary_97
   ) %>% 
   dplyr::filter(value >= 0)  %>% 
   dplyr::rename(
@@ -19,12 +19,51 @@ tidy_data <- original_data %>%
   ) %>% 
   dplyr::select(-variable_id)
 
-columnar_data <- tidy_data %>% 
-  tidyr::pivot_wider(
+yearly_variables_97 <- tidy_data_97 %>% 
+  filter(is.na(job_number)) %>% 
+  select(id, year, variable, value) %>% 
+  pivot_wider(
     names_from = variable,
     values_from = value
   )
 
-test <- columnar_data %>% 
+tidy_data_fix_97 <- tidy_data_97 %>% 
+  filter(!is.na(job_number)) %>% 
+  left_join(yearly_variables_97)
+
+columnar_data_97 <- tidy_data_fix_97 %>% 
+  group_by(id, variable, job_number, year, age) %>% 
+  mutate(row = row_number()) %>%
+  pivot_wider(
+    names_from = variable,
+    values_from = value
+  ) %>% 
+  left_join(cpi)
+
+data_mutations_97 <- columnar_data_97 %>% 
+  mutate(
+    job_satisfaction = dplyr::case_when(
+      job_satisfaction == 5 ~ 1,
+      job_satisfaction == 4 ~ 2,
+      job_satisfaction == 3 ~ 3,
+      job_satisfaction == 2 ~ 4,
+      job_satisfaction == 1 ~ 5,
+      TRUE ~ job_satisfaction
+    )
+  ) %>% 
+  mutate(
+    start_date = as.Date(paste0("1", "/", start_month, "/", start_year), format("%d/%m/%Y")),
+    stop_date = as.Date(paste0("1", "/", stop_month, "/", stop_year), format("%d/%m/%Y")),
+    tenure = (stop_date - start_date) / 365,
+    avg_age_job_year = ((age - tenure) + age) / 2,
+    public = ifelse(industry == 1, 1,0)
+  ) %>% 
+  filter(public == 1 & 
+           !is.na(employer_id) & 
+           !is.na(job_satisfaction))
+
+
+
+test <- columnar_data_97 %>% 
   group_by(id, employer_id, year) %>% 
   summarize(mean_satisfaction = mean(job_satisfaction, na.rm = TRUE))
