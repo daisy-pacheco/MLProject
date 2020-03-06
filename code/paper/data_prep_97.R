22c5dceb849645a06b8befe85593bbbe4f618114
Daisy Pacheco <dpachecobernal@gmail.com>
2020-03-06 14:01:09 +0100

    update_CPI

--- code/paper/data_prep_97.R
+++ code/paper/data_prep_97.R
@@ -52,15 +52,42 @@ data_mutations_97 <- columnar_data_97 %>%
     )
   ) %>% 
   mutate(
+    unique_employer_id = paste0(id, "_", employer_id),
     start_date = as.Date(paste0("1", "/", start_month, "/", start_year), format("%d/%m/%Y")),
     stop_date = as.Date(paste0("1", "/", stop_month, "/", stop_year), format("%d/%m/%Y")),
-    tenure = (stop_date - start_date) / 365,
+    hourly_pay = log((hourly_pay * cpi) + 1),
+    tenure = as.numeric((stop_date - start_date) / 365),
     avg_age_job_year = ((age - tenure) + age) / 2,
     public = ifelse(industry == 1, 1,0)
   ) %>% 
   filter(public == 1 & 
            !is.na(employer_id) & 
-           !is.na(job_satisfaction))
+           !is.na(job_satisfaction)) %>% 
+  select(id, year, job_number, employer_id, 
+         job_satisfaction,
+         age, avg_age_job_year, tenure,
+         hours_worked, hourly_pay, unique_employer_id)
+
+# imputation 
+## linear interpolation for values measured at least twice
+## otherwise, mean imputation
+library(zoo)
+
+imputed_data_97 <- data_mutations_97 %>% 
+  group_by(id) %>% 
+  mutate(tenure = ifelse(is.na(tenure), 
+                         mean(tenure, na.rm = T), 
+                         tenure)) %>% 
+  mutate(hours_worked = ifelse(is.na(hours_worked), 
+                                    mean(hours_worked, na.rm = T), 
+                                    hours_worked)) %>% 
+  mutate(hourly_pay = ifelse(is.na(hourly_pay), 
+                             mean(hourly_pay, na.rm = T), 
+                             hourly_pay)) %>% 
+  ungroup()
+
+imputed_data_final_97 <- imputed_data_97 %>% 
+  filter(hours_worked >= 35)
 
 
 
